f750ffe5448c02e232336e5316729bfa1c75768e
Ivan Stroganov <knowhere@gmail.com>
2012-09-19 20:20:50 -0700

    updating postinstall to be executable

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "creating and migrating database"
 ruby dotcloud_db_setup.rb
 rake db:create
 rake db:migrate
 
+# make sure to add execute permission to this file with chmod+x postinstall.sh
